88c4a6cdc427c88f8c18c9e1f9e5798892529b41
mehmoodkhan007 <mahmood.hassan@kodexolabs.com>
2024-09-02 07:43:32 -0700

    s

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import request_routes
 from app.routes import collection_routes
 from app.routes import response_routes
@@ -6,7 +7,14 @@ from .database import database, engine, Base, metadata
 from .models import collections, requests, params
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
+1
 app.include_router(request_routes.router, prefix="/api")
 app.include_router(collection_routes.router, prefix="/api")
 app.include_router(response_routes.router, prefix="/api")
